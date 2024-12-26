7cf3e71ec821f90c0225ecfd031525e74a84a467
Geovanny Mendoza <mgeovanny@uninorte.edu.co>
2024-12-26 14:15:17 -0500

    V[1.0.1] Update code

--- catalog-service/src/test/java/com/geovannycode/ecommerce/catalog/ProductServiceApplicationTests.java
+++ catalog-service/src/test/java/com/geovannycode/ecommerce/catalog/ProductServiceApplicationTests.java
@@ -5,10 +5,14 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.TestcontainersConfiguration;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Import(TestcontainersConfiguration.class)
+@Testcontainers
+@ActiveProfiles("test")
 class ProductServiceApplicationTests {
 
     @Test
