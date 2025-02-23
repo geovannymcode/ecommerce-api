247fc622e66e88385af98a527ae4d46247cf8956
Geovanny Mendoza <me@geovannycode.com>
2025-02-23 15:47:39 -0500

    Add test controller Close #5

--- catalog-service/src/main/java/com/geovannycode/ecommerce/catalog/infrastructure/adapter/persistence/SpringDataProductRepository.java
+++ catalog-service/src/main/java/com/geovannycode/ecommerce/catalog/infrastructure/adapter/persistence/SpringDataProductRepository.java
@@ -6,8 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SpringDataProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT p FROM Product p WHERE p.code = :code AND p.deleted = false")
     Optional<Product> findByCode(String code);
 
     Page<Product> findAll(Pageable pageable);
@@ -16,6 +18,11 @@ public interface SpringDataProductRepository extends JpaRepository<Product, Long
 
     void deleteByCode(String code);
 
-    @Query("SELECT p FROM Product p WHERE p.name LIKE %:query% OR p.description LIKE %:query%")
-    Page<Product> searchProductsByCriteria(String query, Pageable pageable);
+    @Query("""
+    SELECT p FROM Product p
+    WHERE p.name LIKE %:query%
+    ORDER BY p.name ASC
+""")
+    Page<Product> searchProductsByCriteria(@Param("query") String query, Pageable pageable);
+
 }
--- catalog-service/src/test/java/com/geovannycode/ecommerce/catalog/infrastructure/api/ProductControllerTest.java
+++ catalog-service/src/test/java/com/geovannycode/ecommerce/catalog/infrastructure/api/ProductControllerTest.java
@@ -2,18 +2,25 @@ package com.geovannycode.ecommerce.catalog.infrastructure.api;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.geovannycode.ecommerce.catalog.TestcontainersConfiguration;
+import com.geovannycode.ecommerce.catalog.infrastructure.api.dto.ProductDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Import(TestcontainersConfiguration.class)
@@ -23,6 +30,9 @@ public class ProductControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void shouldGetProducts() throws Exception {
@@ -54,5 +64,49 @@ public class ProductControllerTest {
                 .andExpect(jsonPath("$.status", is(404)))
                 .andExpect(jsonPath("$.title", is("Product Not Found")))
                 .andExpect(jsonPath("$.detail", is("Product with code " + code + " not found")));
+    }
+
+    @Test
+    void shouldSearchProducts() throws Exception {
+        mockMvc.perform(get("/api/products/search")
+                        .param("query", "The")
+                        .param("page", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$.data[0].name").value(is("The Alchemist")));
+    }
+
+    @Test
+    void shouldCreateProduct() throws Exception {
+        ProductDto productDto = new ProductDto(
+                "P115",
+                "New Product",
+                "This is a new product.",
+                "https://example.com/new-product.jpg",
+                new BigDecimal("25.0"),
+                100,
+                null,
+                BigDecimal.ZERO
+        );
+
+        mockMvc.perform(post("/api/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(productDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.code", is("P115")))
+                .andExpect(jsonPath("$.name", is("New Product")));
+    }
+
+    @Test
+    void shouldDeleteProduct() throws Exception {
+
+        mockMvc.perform(get("/api/products/{code}", "P100"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(delete("/api/products/{code}", "P100"))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/products/{code}", "P100"))
+                .andExpect(status().isNotFound());
     }
 }
