461d0c73fa821efa429f972c00dc1e6e52a71a47
Geovanny Mendoza <me@geovannycode.com>
2025-03-01 12:00:00 -0500

    Commit retroactivo

---
+++ order-service/src/main/java/com/geovannycode/ecommerce/order/infrastructure/output/clients/Product.java
@@ -0,0 +1,5 @@
+package com.geovannycode.ecommerce.order.infrastructure.output.clients;
+
+import java.math.BigDecimal;
+
+public record Product(String code, String name, String description, String imageUrl, BigDecimal price, Integer stock, BigDecimal discount) {}
