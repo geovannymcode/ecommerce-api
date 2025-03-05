a5c89deb7aac774b1fd8e0474109bc89035db7b7
Geovanny Mendoza <me@geovannycode.com>
2025-03-04 22:11:56 -0500

    V[1.0.9] Add client payment and catalog

--- order-service/src/main/java/com/geovannycode/ecommerce/order/ApplicationProperties.java
+++ order-service/src/main/java/com/geovannycode/ecommerce/order/ApplicationProperties.java
@@ -5,8 +5,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "app")
 public record ApplicationProperties(
         String catalogServiceUrl,
+        String paymentServiceUrl,
         String orderEventsExchange,
-        String newOrdersQueue,
-        String deliveredOrdersQueue,
-        String cancelledOrdersQueue,
-        String errorOrdersQueue) {}
+        String newOrdersTopic,
+        String deliveredOrdersTopic,
+        String cancelledOrdersTopic,
+        String errorOrdersTopic) {}
